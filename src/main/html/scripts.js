beeeef807781cf0597ba016f6e88b666d33727b3
Adam Koscinski <akoscinski@luxoft.com>
2016-05-16 14:47:44 +0200

    Added html pages for atm service

---
+++ src/main/html/scripts.js
@@ -0,0 +1,17 @@
+function isNumeric(input) {
+    var RE = /^-{0,1}\d*\.{0,1}\d+$/;
+    if(!RE.test(input)){
+        $("#numberError").html("Please provide a proper value");
+        return false;
+    }
+    return true;
+}
+
+function notEmpty() {
+    var name = document.forms["Name"]["name"].value;
+    if(name.length < 1){
+        $("#nameError").html("Please provide the proper name.");
+        return false;
+    }
+    return true;
+}
